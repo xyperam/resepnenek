eaae13a749d92ca05913b33c213978f9accb07ef
Ilham Imani Nur Alam <ilhamimaninuralam@gmail.com>
2021-05-29 22:07:13 +0700

    Detail still in  progress

--- lib/Masakan.dart
+++ lib/Masakan.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:resepnenek/Masakan/Rendang.dart';
 
 class Masakan extends StatelessWidget {
   @override
@@ -12,8 +13,7 @@ class Masakan extends StatelessWidget {
             Row(
               children: <Widget>[
                 Container(
-                  margin: EdgeInsets.all(20.0),
-                  padding: EdgeInsets.only(right: 20.0),
+                  margin: EdgeInsets.all(15.0),
                   decoration: BoxDecoration(
                       borderRadius: BorderRadius.circular(15.0),
                       boxShadow: [
@@ -21,27 +21,229 @@ class Masakan extends StatelessWidget {
                             color: Colors.grey.withOpacity(0.2),
                             spreadRadius: 3.0,
                             blurRadius: 5.0)
-                      ]),
+                      ],
+                      color: Colors.white),
                   child: Column(
                     children: [
+                      Padding(
+                          padding: EdgeInsets.only(left: 120.0, top: 5.0),
+                          child: Row(
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              children: [
+                                Icon(Icons.favorite_border,
+                                    color: Color(0xFFEF7532))
+                              ])),
                       Container(
+                        margin: EdgeInsets.all(10.0),
+                        child: InkWell(
+                          onTap: () => {
+                            Navigator.push(context,
+                                MaterialPageRoute(builder: (context) {
+                              return Rendang();
+                            }))
+                          },
+                        ),
                         padding: EdgeInsets.only(
-                            top: 10.0, bottom: 10.0, left: 10.0, right: 10.0),
-                        height: 200.0,
-                        width: 200.0,
+                            top: 20.0, bottom: 20.0, left: 20.0, right: 20.0),
+                        height: 150.0,
+                        width: 150.0,
                         decoration: BoxDecoration(
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.white,
+                              ),
+                            ],
                             borderRadius: BorderRadius.circular(30.0),
                             image: DecorationImage(
                               image: NetworkImage(
                                   "https://asset.kompas.com/crops/a29yE_hzxM0nJBIId_Lh6aeDkok=/0x3:977x654/780x390/data/photo/2020/06/30/5efaf91e0ec2c.jpg"),
                               fit: BoxFit.fill,
-                              alignment: Alignment.topCenter,
+                              alignment: Alignment.center,
                             )),
                       ),
                       SizedBox(
-                        height: 20.0,
+                        height: 5.0,
                       ),
-                      Text("Rendang")
+                      Text(
+                        "Rendang",
+                        style: TextStyle(fontFamily: 'Varela', fontSize: 15.0),
+                      ),
+                      SizedBox(
+                        height: 10.0,
+                      ),
+                    ],
+                  ),
+                ),
+
+                // row 2
+                Container(
+                  margin: EdgeInsets.all(15.0),
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15.0),
+                      boxShadow: [
+                        BoxShadow(
+                            color: Colors.grey.withOpacity(0.2),
+                            spreadRadius: 3.0,
+                            blurRadius: 5.0)
+                      ],
+                      color: Colors.white),
+                  child: Column(
+                    children: [
+                      Padding(
+                          padding: EdgeInsets.only(left: 120.0, top: 5.0),
+                          child: Row(
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              children: [
+                                Icon(Icons.favorite_border,
+                                    color: Color(0xFFEF7532))
+                              ])),
+                      Container(
+                        margin: EdgeInsets.all(10.0),
+                        child: InkWell(),
+                        padding: EdgeInsets.only(
+                            top: 20.0, bottom: 20.0, left: 20.0, right: 20.0),
+                        height: 150.0,
+                        width: 150.0,
+                        decoration: BoxDecoration(
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.white,
+                              ),
+                            ],
+                            borderRadius: BorderRadius.circular(30.0),
+                            image: DecorationImage(
+                              image: NetworkImage(
+                                  "https://asset.kompas.com/crops/a29yE_hzxM0nJBIId_Lh6aeDkok=/0x3:977x654/780x390/data/photo/2020/06/30/5efaf91e0ec2c.jpg"),
+                              fit: BoxFit.fill,
+                              alignment: Alignment.center,
+                            )),
+                      ),
+                      SizedBox(
+                        height: 5.0,
+                      ),
+                      Text(
+                        "Rendang",
+                        style: TextStyle(fontFamily: 'Varela', fontSize: 15.0),
+                      ),
+                      SizedBox(
+                        height: 10.0,
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              children: <Widget>[
+                Container(
+                  margin: EdgeInsets.all(15.0),
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15.0),
+                      boxShadow: [
+                        BoxShadow(
+                            color: Colors.grey.withOpacity(0.2),
+                            spreadRadius: 3.0,
+                            blurRadius: 5.0)
+                      ],
+                      color: Colors.white),
+                  child: Column(
+                    children: [
+                      Padding(
+                          padding: EdgeInsets.only(left: 120.0, top: 5.0),
+                          child: Row(
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              children: [
+                                Icon(Icons.favorite_border,
+                                    color: Color(0xFFEF7532))
+                              ])),
+                      Container(
+                        margin: EdgeInsets.all(10.0),
+                        child: InkWell(),
+                        padding: EdgeInsets.only(
+                            top: 20.0, bottom: 20.0, left: 20.0, right: 20.0),
+                        height: 150.0,
+                        width: 150.0,
+                        decoration: BoxDecoration(
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.white,
+                              ),
+                            ],
+                            borderRadius: BorderRadius.circular(30.0),
+                            image: DecorationImage(
+                              image: NetworkImage(
+                                  "https://asset.kompas.com/crops/a29yE_hzxM0nJBIId_Lh6aeDkok=/0x3:977x654/780x390/data/photo/2020/06/30/5efaf91e0ec2c.jpg"),
+                              fit: BoxFit.fill,
+                              alignment: Alignment.center,
+                            )),
+                      ),
+                      SizedBox(
+                        height: 5.0,
+                      ),
+                      Text(
+                        "Rendang",
+                        style: TextStyle(fontFamily: 'Varela', fontSize: 15.0),
+                      ),
+                      SizedBox(
+                        height: 10.0,
+                      ),
+                    ],
+                  ),
+                ),
+
+                //Row 2
+                Container(
+                  margin: EdgeInsets.all(15.0),
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15.0),
+                      boxShadow: [
+                        BoxShadow(
+                            color: Colors.grey.withOpacity(0.2),
+                            spreadRadius: 3.0,
+                            blurRadius: 5.0)
+                      ],
+                      color: Colors.white),
+                  child: Column(
+                    children: [
+                      Padding(
+                          padding: EdgeInsets.only(left: 120.0, top: 5.0),
+                          child: Row(
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              children: [
+                                Icon(Icons.favorite_border,
+                                    color: Color(0xFFEF7532))
+                              ])),
+                      Container(
+                        margin: EdgeInsets.all(10.0),
+                        child: InkWell(),
+                        padding: EdgeInsets.only(
+                            top: 20.0, bottom: 20.0, left: 20.0, right: 20.0),
+                        height: 150.0,
+                        width: 150.0,
+                        decoration: BoxDecoration(
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.white,
+                              ),
+                            ],
+                            borderRadius: BorderRadius.circular(30.0),
+                            image: DecorationImage(
+                              image: NetworkImage(
+                                  "https://asset.kompas.com/crops/a29yE_hzxM0nJBIId_Lh6aeDkok=/0x3:977x654/780x390/data/photo/2020/06/30/5efaf91e0ec2c.jpg"),
+                              fit: BoxFit.fill,
+                              alignment: Alignment.center,
+                            )),
+                      ),
+                      SizedBox(
+                        height: 5.0,
+                      ),
+                      Text(
+                        "Rendang",
+                        style: TextStyle(fontFamily: 'Varela', fontSize: 15.0),
+                      ),
+                      SizedBox(
+                        height: 10.0,
+                      ),
                     ],
                   ),
                 )
